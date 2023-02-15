eea1d6fc8324fe3cb39d2bd425c1a67f6291db8d
Charlotte McCanna <charliegmccanna@gmail.com>
2023-02-15 05:20:31 +0000

    Completed sql-join exercise.

---
+++ sql-join/where-are-they.sql
@@ -0,0 +1,7 @@
+select "addresses"."line1",
+       "cities"."name" as "cityName",
+       "addresses"."district",
+       "countries"."name" as "countryName"
+  from "addresses"
+  join "cities" using ("cityId")
+  join "countries" using ("countryId");
